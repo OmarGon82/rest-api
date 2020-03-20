5a00a75aa74a8e4a78b65db07e2c14b09f4bf7e1
Omar Gonzalez <gonzalezoe82@gmail.com>
2020-03-20 21:23:36 +0900

    post route to create user functional

--- routes.js
+++ routes.js
@@ -1,20 +1,19 @@
 'use strict';
 
 /**
- * Importing express and express Router. 
- * Importing models using destructuring.
+ * Import: express and express Router. 
+ * Import: models (using destructuring).
  */
 const express =  require('express');
 const router = express.Router();
 const { User, Course } = require('./models')
 
 
-// Importing methods from express-validator
+// Import: methods from express-validator
 const {check, validationResult } = require('express-validator')
 
-const nameValidator = check('name')
-    .exists({ checkFalsy: true, checkNull: true })
-    .withMessage('Please providea value for "name"');
+// Import: bcrypts module;
+const bcryptjs = require('bcryptjs');
 
 
 /**
@@ -34,16 +33,53 @@ function handleAsync(cb) {
     /**
      * GET route to get all the users information from the database
      */
-   router.get('/users', handleAsync(async (req, res) => {
-    const users = await User.findAll();
+    router.get('/users', 
+    // [
+    //     check('firstName')
+    //         .exists({ checkFalsy: true, checkNull: true})
+    //         .withMessage('Please provide a value for "firstName"'),
+    //     check('lastName')
+    //         .exists({ checkFalsy: true, checkNull: true})
+    //         .withMessage('Please provide a value for "lastName"'),
+    //     check('emailAddress')
+    //         .exists({ checkFalsy: true, checkNull: true})
+    //         .withMessage('Please provide a value for "emailAddress"'),
+    //     check('password')
+    //         .exists({ checkFalsy: true, checkNull: true})
+    //         .withMessage('Please provide a value for "password"'),
+    //    ],
+        handleAsync(async (req, res) => {
+              
+        // Attempt to get the validation  result from the Request  object.
+        const errors = validationResult(req)
+    
+        /**
+         * isEmpty returns true if there are no errors so to check if there are errors we use !(NOT operator)
+         * If there are validation errors
+         */
+        if (!errors.isEmpty()) {
+            const errorMessage = errors.array().map(error => error.msg);
+    
+            // return the error to the client
+            return res.status(400).json({ errors: errorMessage})
+        }
+    
+        // Get user from the request body.
+        const users = await User.findAll();
+
+        // // Hash the new user's password.
+        // user.password = bcryptjs.hashSync(user.password);
+
         res.json(users.map(user=> user.get({ plain: true })))
-       
-   }));
+        res.status(201).end();
+           
+       }));
 
    /**
-    * Post route to validate All users
+    * Post route to create a new User
     */
-   router.post('/users', [
+   router.post('/users', 
+   [
     check('firstName')
         .exists({ checkFalsy: true, checkNull: true})
         .withMessage('Please provide a value for "firstName"'),
@@ -53,7 +89,11 @@ function handleAsync(cb) {
     check('emailAddress')
         .exists({ checkFalsy: true, checkNull: true})
         .withMessage('Please provide a value for "emailAddress"'),
-   ], handleAsync(async (req, res) => {
+    check('password')
+        .exists({ checkFalsy: true, checkNull: true})
+        .withMessage('Please provide a value for "password"'),
+   ], 
+   handleAsync(async (req, res) => {
           
     // Attempt to get the validation  result from the Request  object.
     const errors = validationResult(req)
@@ -67,14 +107,32 @@ function handleAsync(cb) {
 
         // return the error to the client
         return res.status(400).json({ errors: errorMessage})
-    }
+    } else {
+        // Get user from the request body.
+        const user = req.body
+        // Hash the new user's password.
+        user.password = bcryptjs.hashSync(user.password);
+        await User.create(user)
+        console.log("user successfully created!")
+        res.status(201).location('/').end();
+    }     
+}));
 
-    // Get user from the request body.
-    const users = await User.findAll();
-        res.json(users.map(user=> user.get({ plain: true })))
-        res.status(201).end();
-       
-   }));
+//send a DELETE request to /user/:id to DELETE a user.
+// router.delete("/users/:id", handleAsync( async(req, res, next) => {
+//     const user = await User.findByPk(req.params.id);
+//     if(user) {
+//         await User.destroy({
+//             where: {
+//                 id: user.id
+//             }
+//         })
+//         res.status(204).end();
+//     } else {
+//         res.status(404).json({message: "user not found"})
+//     }
+
+// }));
 
    router.get('/courses', handleAsync(async (req, res) => {
     const courses = await Course.findAll();
